7038bcedefbb39f4bf5d00ba2b68d59e7f4e3f45
Michael Kopchenov <121085772+MichaelKopchenov@users.noreply.github.com>
2024-01-17 12:44:44 +0300

    code refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react';
-import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio';
-import Contacts from '../Contacts/Contacts';
-import Navigtion from '../Navigation/Navigation';
-import './Header.css';
+import { useState } from "react";
+import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
+import Contacts from "../Contacts/Contacts";
+import Navigtion from "../Navigation/Navigation";
+import Footer from "../Footer/Footer";
+import "./Header.css";
 
 export default function Header() {
   const [isShown, setIsShown] = useState(false);
 
-  const handleClick = event => {
-    setIsShown(current => !current);
+  const handleClick = (event) => {
+    setIsShown((current) => !current);
   };
 
   return (
     <>
       {isShown ? <Navigtion /> : null}
-        <section className="header__main">
-          <div className='header__container'>
-            <div className="header__about">
-              <h1 className="header__title">
-                Портфолио Веб-разработчика<br /> Копченова Михаила Викторовича.
-              </h1>
-              <p className="header__subtitle">
-                Нажми на кнопку, чтобы узнать больше про мои работы и меня.
-              </p>
-              <div className="header__logo" />
-              <a href='#AboutMe'>
-                <button onClick={handleClick} className="header__link">
-                  Узнать больше
-                </button>
-              </a>
-            </div>
+      <section className="header__main">
+        <div className="header__container">
+          <div className="header__about">
+            <h1 className="header__title">
+              Портфолио Веб-разработчика
+              <br /> Копченова Михаила Викторовича.
+            </h1>
+            <p className="header__subtitle">
+              Нажми на кнопку, чтобы узнать больше про мои работы и меня.
+            </p>
+            <div className="header__logo" />
+            <a href="#AboutMe">
+              <button onClick={handleClick} className="header__link">
+                Узнать больше
+              </button>
+            </a>
           </div>
-        </section>
-      {isShown 
-        ? (<> 
-              <AboutMe /> 
-              <Portfolio /> 
-              <Contacts /> 
-          </>) 
-        : null}
+        </div>
+      </section>
+      {isShown ? (
+        <>
+          <AboutMe />
+          <Portfolio />
+          <Contacts />
+        </>
+      ) : null}
+      <Footer />
     </>
   );
-};
+}
--- src/utils/urlConstants.js
+++ src/utils/urlConstants.js
@@ -1,26 +1,26 @@
 const urlLink = [
   {
     title: 'Project "Learn to learn"',
-    way: 'https://MichaelKopchenov.github.io/how-to-learn/'
+    way: "https://MichaelKopchenov.github.io/how-to-learn/",
   },
   {
     title: 'Project "Traveling around Russia"',
-    way: 'https://MichaelKopchenov.github.io/russian-travel/'
+    way: "https://MichaelKopchenov.github.io/russian-travel/",
   },
   {
-    title:'Project "Mesto"',
-    way: 'https://MichaelKopchenov.github.io/react-mesto-auth/'
+    title: 'Project "Mesto"',
+    way: "https://MichaelKopchenov.github.io/react-mesto-auth/",
   },
   {
     title: 'Project "Fast Digital Dicisions"',
-    way: 'https://fast-dd.ru/'
+    way: "https://michaelkopchenov.github.io/portfolio/",
   },
   {
     title: 'Project "Vektor" (Without adaptive)',
-    way: 'https://michaelkopchenov.github.io/vektor/'
+    way: "https://michaelkopchenov.github.io/vektor/",
   },
-]
+];
 
 module.exports = {
-  urlLink
+  urlLink,
 };
